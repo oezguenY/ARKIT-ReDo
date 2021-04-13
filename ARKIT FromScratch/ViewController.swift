062bf58bf77d98948546ec5a04f90fd77545b1ec
Özgün Yildiz <ozgun.yildiz@hotmail.de>
2021-04-13 20:46:48 +0200

    Recreated the default ARKit app by Apple.

--- ARKIT FromScratch/ViewController.swift
+++ ARKIT FromScratch/ViewController.swift
@@ -15,8 +15,21 @@ class ViewController: UIViewController, ARSCNViewDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        // the ViewController is the delegate of the sceneView
         sceneView.delegate = self
         sceneView.debugOptions = [.showFeaturePoints]
+        
+        // we are providing a path to our ship in our art_scnassets
+        // normally, this is being force unwrapped, which is actually safe because we can see our scnassets folder and our ship scene. Nevertheless, getting used to force unwrapping is dangerous and its better to be safe than sorry :D
+        if let scene = SCNScene(named: "art.scnassets/ship.scn") {
+            sceneView.scene = scene
+        }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+        let configuration = ARWorldTrackingConfiguration()
+        sceneView.session.run(configuration)
     }
 
 
